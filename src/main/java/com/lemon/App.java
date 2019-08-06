afc1c78a39f2856b94901e84a67dcd8fc33ab699
lemon <1782579945@qq.com>
2019-08-07 00:39:22 +0800

    SpringBoot完成基础项目搭建

--- src/main/java/com/lemon/App.java
+++ src/main/java/com/lemon/App.java
@@ -1,7 +1,12 @@
 package com.lemon;
 
+import com.lemon.dao.UserDOMapper;
+import com.lemon.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration //将该启动类当成一个自动化可以配置的一个bean，并且能够开启整个工程类的基于springboot一个自动化的配置
+
+/*@EnableAutoConfiguration //将该启动类当成一个自动化可以配置的一个bean，并且能够开启整个工程类的基于springboot一个自动化的配置*/
+//SpringBootApplication和 EnableAutoConfiguration功能类似
+@SpringBootApplication(scanBasePackages = {"com.lemon"})
 @RestController   // @RestController  = @Controller +  @RequestBody
+@MapperScan("com.lemon.dao")   //dao存放
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String home(){
 
-        return "hello world!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
     }
     public static void main( String[] args )
     {
