e3247d15d9e80d39bb1ab5e42368ac4586f5073d
lemon <1782579945@qq.com>
2019-08-08 09:13:41 +0800

    定义通用的返回对象-异常处理01

--- src/main/java/com/lemon/controller/UserController.java
+++ src/main/java/com/lemon/controller/UserController.java
@@ -8,10 +8,11 @@ import com.lemon.service.UserService;
 import com.lemon.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author lemonsun
@@ -46,5 +47,16 @@ public class UserController {
         UserVO userVO = new UserVO();
         BeanUtils.copyProperties(userModel,userVO);
         return userVO;
+    }
+
+    //定义exceptionhandler解决 未被controller层吸收的exception
+    @ExceptionHandler(Exception.class)  //定义收到什么样的exception之后，才会进入它的处理环节
+    @ResponseStatus(HttpStatus.OK)  // 返回http status 200
+    public Object handlerException(HttpServletRequest request,Exception ex){
+        /*handlerException仅仅只能返回页面的路径 无法返回业务处理类返回的ResponseBody这种形式 还需要解决一层问题*/
+        CommonReturnType commonReturnType = new CommonReturnType();
+        commonReturnType.setStatus("fail");
+        commonReturnType.setData(ex);
+        return commonReturnType;
     }
 }
